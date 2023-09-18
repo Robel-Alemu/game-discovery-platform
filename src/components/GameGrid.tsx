296e628a01d7e4902bf0be74748d483b9dfaa266
Robel <robelalemu2372@gmail.com>
2023-09-18 17:15:01 +0300

    created loading effect using skeleton

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,24 +1,21 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
 import useFetchGames from "../hooks/UseFetchGames";
-import { SimpleGrid, Spinner, Text } from "@chakra-ui/react";
+import { SimpleGrid, Skeleton, Spinner, Text } from "@chakra-ui/react";
 import GameCard from "./GameCard";
+import GameCardSkeletons from "./GameCardSkeletons";
 
 const GameGrid = () => {
   const { games, isLoading, error } = useFetchGames();
+  const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (isLoading) {
-    return <Spinner />;
-  }
   return (
     <>
       {error && <Text>{error}</Text>}
 
-      <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
-        padding="10px"
-        spacing={10}
-      >
+      <SimpleGrid columns={{ sm: 1, md: 2, lg: 3 }} padding="10px" spacing={10}>
+        {isLoading &&
+          skeletons.map((skeleton) => <GameCardSkeletons key={skeleton} />)}
         {games.map((game) => (
           <GameCard key={game.id} game={game} />
         ))}
